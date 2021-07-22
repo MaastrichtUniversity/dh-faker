636d4d5082edf24913962cc9c1ab1e05d93db875
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2021-07-22 11:29:44 +0200

    Initial commit

---
+++ create_fake_data.py
@@ -0,0 +1,104 @@
+from irodsrulewrapper.rule import RuleManager
+from faker import Faker
+import os
+import json
+
+fake = Faker("nl_NL")
+
+user = "jmelius"
+user_email = "fake-email@maastrichtuniversity.nl"
+data_steward = "opalmen"
+number_of_projects = 1
+number_of_collections_per_project = 5
+
+
+def create_file(directory, category='text', nb_sentences=5):
+    file_name = fake.file_name(category=category)
+    f = open(directory + "/" + file_name, "a")
+    f.write(fake.paragraph(nb_sentences=nb_sentences))
+    f.close()
+    print(file_name)
+
+
+def create_dir(token, depth=5, category='text'):
+    ingest_zone = os.path.join("/mnt/ingest/zones/", token)
+    full_path = fake.file_path(depth=depth, category=category)
+    path, file = os.path.split(full_path)
+    path = ingest_zone + path
+    os.makedirs(path, exist_ok=True)
+    print(path)
+    return path
+
+
+def create_project():
+    manager = RuleManager()
+    project = manager.create_new_project("1-1-2018", "1-1-2018",
+                                         "iresResource", "replRescUM01", 42, fake.catch_phrase(), user,
+                                         data_steward, "XXXXXXXXX", "false", "false")
+    manager.set_acl('default', 'own', data_steward, project.project_path)
+    manager.set_acl('default', 'own', user, project.project_path)
+    manager.set_acl('default', 'read', "datahub", project.project_path)
+    return project
+
+
+def create_contacts(amount):
+    users = []
+    for counter in range(amount):
+        user = {}
+        user['FirstName'] = fake.first_name()
+        user['LastName'] = fake.last_name()
+        user['MidInitials'] = ""
+        user['Email'] = fake.ascii_company_email()
+        user['Phone'] = fake.phone_number()
+        user['Address'] = fake.address().replace("\n", " ")
+        user['Affiliation'] = fake.company()
+        user['Role'] = fake.job()
+        users.append(user)
+
+    return json.dumps(users)
+
+
+def create_collection(project_id):
+    data = {
+        "user": user,
+        "creator": user_email,
+        "project": project_id,
+        "title": fake.catch_phrase(),
+        "description": fake.sentence(),
+        "date": fake.date(),
+        "articles": '10.1016/j.cell.2021.02.021,10.1126/science.abc4346',
+        "organism_id": "ncbitaxon:http://purl.obolibrary.org/obo/NCBITaxon_9606",
+        "organism_label": "Homo sapiens",
+        "tissue_id": "efo:http://www.ebi.ac.uk/efo/EFO_0000803",
+        "tissue_label": "renal system",
+        "technology_id": "ero:http://purl.obolibrary.org/obo/ERO_0000570",
+        "technology_label": "heart perfusion",
+        "factors": [fake.word(), fake.word(), fake.word(), fake.word()],
+        "contacts": create_contacts(3)
+    }
+    token = RuleManager().create_drop_zone(data)
+    print(token)
+    return token
+
+
+def ingest_collection(token):
+    RuleManager().start_ingest(user, token)
+
+
+def main():
+    for x in range(number_of_projects):
+        project = create_project()
+        print(project.project_id)
+        for y in range(number_of_collections_per_project):
+            token = create_collection(project.project_id)
+            directory = create_dir(token)
+            create_file(directory)
+            directory = create_dir(token, depth=3, category='video')
+            create_file(directory, category='video', nb_sentences=100)
+            directory = create_dir(token, depth=1, category='office')
+            create_file(directory, category='office', nb_sentences=250)
+            ingest_collection(token)
+
+
+if __name__ == "__main__":
+    main()
